36a606ebefb91db4503caa984fb3fff671e7029f
Christopher Lam <christopher.lam@students.mq.edu.au>
2017-12-01 02:39:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 const app = "I don't do much."
+
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendKitten(name){
+  kittens.push(name)
+  return kittens
+}
+
+function appendKitten(name){
+  var arr2 = [...kittens, name]
+  return arr2
+}
+
+function prependKitten(name){
+  var arr2 = [name, ...kittens]
+  return arr2
+}
+
+function destructivelyPrependKitten(name){
+  kittens.unshift(name)
+  return kittens
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop()
+  return kittens
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift()
+  return kittens
+}
+
+function removeLastKitten() {
+  kittens.slice(0, iceCreams.length - 1)
+  return kittens
+}
+
+function removeFirstKitten() {
+  var arr2 = [kittens]
+  new_arr = arr2.slice(1)
+  return new_arr
+}
